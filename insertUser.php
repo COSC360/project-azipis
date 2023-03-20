a91eb8a95037a06d98262e4d57d8a40fa306c1ac
azipis <andrei_zipis@hotmail.com>
2023-03-19 20:02:09 -0700

    Images now stored in user folders.

--- insertUser.php
+++ insertUser.php
@@ -9,13 +9,19 @@ $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['pw'];
 $description = 'test';
-$avatarimgpath = 'images/'.$username.'/'.$_FILES['img'];
+$avatarimgpath = 'images/'.$username.'/'.$_FILES['img']['name'];
 $firstname = $_POST['fname'];
 $lastname = $_POST['lname'];
 
 $result = php_insert($query, $types, $username, $email, $password, $description, $avatarimgpath, $firstname, $lastname);
 
 if ($result) {
+    if (!file_exists('images/'.$username)) {
+        mkdir('images/'.$username, 0777, true);
+    }
+    move_uploaded_file($_FILES['img']['tmp_name'], $avatarimgpath);
+
+
     echo "Insertion succeeded";
 } else {
     echo "Insertion failed";
