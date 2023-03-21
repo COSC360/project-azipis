0c9074b30d56e6c6edcaf03d50f83ccd9625a9ff
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-03-20 23:10:05 -0700

    Add comments

--- sql.php
+++ sql.php
@@ -1,12 +1,17 @@
 <?php include_once 'connection.php';?>
 
 <?php
+
+//For sql selection queries
+//RETURN: FALSE for failure, RESULT OBJECT for success
 function php_select($query) {
     $conn = getConnection();
     $result = mysqli_query($conn, $query);
     return $result;
 }
 
+//For sql insertion queries
+//RETURN: FALSE for failure, TRUE for success
 function php_insert($query, $types, ...$binds) {
     $conn = getConnection();
     // Prepare the SQL statement
@@ -28,6 +33,10 @@ function php_insert($query, $types, ...$binds) {
     return $success;
 }
 
+
+//For sql queries that have a auto-increment id
+//Should be called immediately after an insert statement
+//RETURN: value generated for an AUTO_INCREMENT column by the last query or 0 for failure
 function php_get_last_insert_id() {
     $conn = getConnection();
     $result = mysqli_insert_id($conn);
