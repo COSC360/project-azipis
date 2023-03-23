a1813640fa61db05425026871f36a06f0baab584
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-03-23 13:53:25 -0700

    Fixed extra thread bug

--- community.php
+++ community.php
@@ -84,35 +84,6 @@ session_start(); ?>
          <div id="threads">
             <?php include 'thread_template.php' ?>
          </div>
-               <a class="thread" href="">
-
-                  <figure>
-                     <span class="circle"></span>
-                     <img src="images/coffeecup.png" alt="coffee cup" class="overlayed">
-                  </figure>
-
-                  <div class="thread-info">
-
-                     <h2 class="thread-name"></h2>
-
-                        <p>
-                           <span class="username"></span>
-                           <span class="date"></span>
-                           <span class="community"></span>
-                        </p>
-
-                  </div>
-
-
-            </a>
-               <div class="points">
-                  <form action="vote.php" method="post"></form>
-                     <button>^</button>
-                     <p class="pointnum">0</p>
-                     <button>v</button>
-                  </div>
-            </div>
-            </div>
 
 
       </article>
